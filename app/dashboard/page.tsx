fe20d2be0734a03c594875de0f973389ee6769c6
Subodh <subodh16728@gmail.com>
2024-07-08 22:22:31 +0530

    Removed duplicate seeded data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/app/lib/data";
 
 export default async function Page() {
-  // just a comment
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
