701315060b44085f1fb23224d567149ae93da5bd
Subodh Yadav <subodh16728@gmail.com>
2024-07-14 17:09:31 +0530

    Added missing error

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -121,15 +121,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
-              <div id="status-error" aria-live="polite" aria-atomic="true">
-                {state.errors?.status &&
-                  state.errors.status.map((error: string) => (
-                    <p className="mt-2 text-sm text-red-500" key={error}>
-                      {error}
-                    </p>
-                  ))}
-              </div>
             </div>
+            <div id="status-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.status &&
+                state.errors.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
+            {state.message && (
+              <p className="mt-2 text-sm text-red-500">{state.message}</p>
+            )}
           </div>
         </fieldset>
       </div>
